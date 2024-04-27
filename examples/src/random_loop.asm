911e31f8389ed29c8b4b9bd98120fd9b13b06293
Jaxson Pahukula <jaxpahu@gmail.com>
2024-04-27 18:39:00 -0400

    random loop example

---
+++ examples/src/random_loop.asm
@@ -0,0 +1,33 @@
+# This program generates a random number and displays it on the screen every second
+6401
+653C
+
+# generating random number
+C0FF
+A300
+F033
+
+# drawing number
+6301
+F265
+
+F029
+D345
+
+7305
+F129
+D345
+
+7305
+F229
+D345
+
+# waiting one second
+F515
+F007
+3000
+1220
+
+00E0
+# infinite loop
+1204
