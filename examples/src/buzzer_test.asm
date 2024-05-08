37d4d1ab6d482f85db71d65e1224dedf923fa8f1
Jaxson Pahukula <jaxpahu@gmail.com>
2024-05-08 09:17:24 -1000

    buzzer example

---
+++ examples/src/buzzer_test.asm
@@ -0,0 +1,27 @@
+# loading audio icon sprite
+6002 6129 62E5 63E5 64E5 65E5 6629 6702
+A000 F855
+
+603C 6101 6201
+
+# start buzzer and draw sprite
+F018
+D128
+
+# wait one second
+F015
+FA07
+3A00
+1220
+
+# erase sprite
+D128
+
+# wait one second
+F015
+FA07
+3A00
+122A
+
+# loop
+1200
